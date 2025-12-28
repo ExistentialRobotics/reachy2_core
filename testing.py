b6e73d5a4687c12c9426c72d2b0837cc5a5385c4
Darrengn <ngwdarren@gmail.com>
2025-12-27 18:14:08 -0800

    code for moving head

--- testing.py
+++ testing.py
@@ -4,6 +4,6 @@ reachy = ReachySDK(host='localhost')
 reachy.head.turn_on()
 
 reachy.head.rotate_by(roll=0, pitch=30, yaw=0, frame='head')
-reachy.head.rotate_by(roll=0, pitch=-50, yaw=0, frame='head')
+# reachy.head.rotate_by(roll=0, pitch=-50, yaw=0, frame='head')
 
 reachy.turn_off_smoothly()
